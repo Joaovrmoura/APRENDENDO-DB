97565cc2b274ad1c9e4e5c706ef0b15375a00f53
Joaovrmoura <joaovrmoura4@gmail.com>
2023-12-10 05:04:07 -0300

    fisrt

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+      
+
+    <title> Cadastro</title>
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
